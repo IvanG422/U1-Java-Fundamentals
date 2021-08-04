cc8be01aa9a5789a67de08d3074ba8586cf37e79
IvanG422 <guerriva000@warren.k12.in.us>
2021-08-04 11:53:21 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,25 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args) {
+        double num1 = 3.4;
+        boolean num2 = false;
+        double num3 = 4.5;
+
+        String first_Name = "George";
+        int age = 16;
+        System.out.println(first_Name);
+        System.out.println(age);
+
+        String firstName = "George ";
+        String lastName = "George";
+        int accountID = 21578232;
+        double balance = 05.23;
+
+        System.out.println(firstName + lastName);
+        System.out.println("ID Number: " + accountID);
+        System.out.println("Your balance is: " + balance);
+
+    }
+
 }
